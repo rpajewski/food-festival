2ca73e696f1404b618bd1b799f4184501fc285c1
Ricky Bobby <richard.pajewski@gmail.com>
2021-01-22 15:27:08 -0600

    minor tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-require("bootstrap");
-
+require('bootstrap');
 const img1 = require("../../assets/img/food-table.jpg");
 const img2 = require("../../assets/img/grill.jpg");
 
@@ -20,8 +19,8 @@ $(document).ready(function() {
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
-  ]
-
+  ];
+  
   carouselSlides.forEach((slide, i) => {
     $('.carousel-inner').append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
